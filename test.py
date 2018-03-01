678252b740d2a33172834081451fe74300560819
EC2 Default User <ec2-user@ip-172-31-2-162.us-east-2.compute.internal>
2018-03-01 20:22:14 +0000

    add test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import time
+import math
+
+def test(array):
+    if len(array) < 18:
+        time.sleep(.001)
+        test(array + ['T']);
+        test(array + ['F']);
+    else:
+        runAlgorithm(array)
+
+def runAlgorithm(array):
+    global i
+    i += 1
+    print(str(format((i/262144.0 * 100.0), '.6f')) + "%\t" + str(array))
+
+
+i = 0.0
+startTime = time.time();
+test([]);
+endTime = time.time()
+print('Time Elapsed: ' + str(endTime - startTime) + " sec (" + str((endTime - startTime)/60) + " min)");
