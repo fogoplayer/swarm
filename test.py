2b45c57ecfda84e301df5c8a37a1a0df94a607e5
EC2 Default User <ec2-user@ip-172-31-2-162.us-east-2.compute.internal>
2018-03-01 22:14:56 +0000

    Added comments to test.py

--- test.py
+++ test.py
@@ -4,14 +4,19 @@ import math
 i = 0.0
 
 def test(array):
+    #if array is not at full size, keep creating branches using recursion
+    #1ms delay to prevent lag
     if len(array) < 18:
         time.sleep(.001)
         test(array + ['T']);
         test(array + ['F']);
     else:
+        #if array is complete, execute algorithm
         runAlgorithm(array)
 
 def runAlgorithm(array):
+    
+    #Print out % completion through all permutations and the list of parking space states
     global i
     i += 1
     print(str(format((i/262144.0 * 100.0), '.6f')) + "%\t" + str(array))
