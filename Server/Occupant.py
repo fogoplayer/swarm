efa7601cb729834833338c77956487e4bc81633d
EC2 Default User <ec2-user@ip-172-31-2-162.us-east-2.compute.internal>
2018-03-13 16:25:40 +0000

    Condensed down to two python files

--- Server/Occupant.py
+++
@@ -1,6 +0,0 @@
-class Occupant:
-    id = "";
-    nextInstruction = "";
-    
-    def __init__(self, id):
-        self.id = id;
---
+++ Server/runner.py
@@ -0,0 +1,7 @@
+import main
+
+
+runTest();
+#initializeLot([False,True,False,True,False,True,False,True,False,True,False,True,False,True,False,True,False, True])
+#for i in range(0,6):
+        #print(virtualLot[i]);
