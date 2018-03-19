762183b8eb202f7ca63064b3677a395827f7c4f0
EC2 Default User <ec2-user@ip-172-31-2-162.us-east-2.compute.internal>
2018-03-19 16:56:56 +0000

    Added Children to Spot constructors Made progress on algorithm

--- Server/algorithm.py
+++ Server/algorithm.py
@@ -10,7 +10,22 @@ def mainFunction(vLot, occ):
     occupants = occ
     newLot = virtualLot
     
-    #Move all cars to their default states
-    for y in virtualLot:
-        for x in virtualLot:
-            moveCar()
+    #Move all cars to their default states, starting at exit point
+    moveTreeToDefaultStates([0,2])
+
+def moveTreeToDefaultStates(coordinates):
+    x = coordinates[1]
+    y = coordinates[0]
+    if(virtualLot[y,x].getOccupantID != None):
+        moveCar(y,x);
+        #TODO Add recursion
+
+def moveCar(childY,childX):
+    child = virtualLot[childY,childX]
+    parentCoordinates = child.getParent()
+    parentY = parentCoordinates[0]
+    parentX = parentCoordinates[1]
+    if virtualLot[parentY,parentX].getOccupantID == None:
+        virtualLot[parentY,parentX].setOccupantID(child.getOccupantID)
+        #TODO detect if two cars are trying to get to the same spot
+    
