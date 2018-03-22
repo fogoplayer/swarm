2ffb16524106c16582d060433c57f03c33ad1f88
EC2 Default User <ec2-user@ip-172-31-2-162.us-east-2.compute.internal>
2018-03-22 16:39:01 +0000

    Proving github is a thing

--- Server/algorithm.py
+++ Server/algorithm.py
@@ -1,31 +1,50 @@
 #main function
 
-virtualLot=[];
+virtualLot=[]
+newLot = []
 occupants=[]
 
 def mainFunction(vLot, occ):
     global virtualLot
     global occupants
+    global newLot
     virtualLot = vLot
     occupants = occ
     newLot = virtualLot
     
     #Move all cars to their default states, starting at exit point
-    moveTreeToDefaultStates([0,2])
-
-def moveTreeToDefaultStates(coordinates):
-    x = coordinates[1]
-    y = coordinates[0]
-    if(virtualLot[y,x].getOccupantID != None):
-        moveCar(y,x);
-        #TODO Add recursion
-
-def moveCar(childY,childX):
-    child = virtualLot[childY,childX]
-    parentCoordinates = child.getParent()
-    parentY = parentCoordinates[0]
-    parentX = parentCoordinates[1]
-    if virtualLot[parentY,parentX].getOccupantID == None:
-        virtualLot[parentY,parentX].setOccupantID(child.getOccupantID)
-        #TODO detect if two cars are trying to get to the same spot
+    tellAllCarsToGo()
+    algorithm()
+    print ""
+    for y in newLot:
+            printString = ""
+            for x in y:
+                printString += x.toString()
+            print printString
+            
     
+    return newLot
+
+def tellAllCarsToGo():
+    for spot in occupants:
+        if spot.occupantID != None:
+            spot.setGoing(True)
+
+def algorithm():
+    contestedDestinations = allClear()
+    if(len(contestedDestinations) == 0):
+        #TODO Push new lot
+        print "All clear!"
+    else:
+        for dest in contestedDestinations:
+            pickWinner
+        algorithm()
+    
+def allClear():
+    currentDestinations = []
+    contestedDestinations = []
+    for spot in occupants:
+        if spot.isGoing() and (spot.getDestination() not in occupants) and (spot.getDestination not in contestedDestinations):
+            currentDestinations.append(spot.getParent())
+    print "Contested Destinations: " + str(contestedDestinations)
+    return contestedDestinations
