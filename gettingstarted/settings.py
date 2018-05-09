7459f8c70f59e0b4685dcf577533c6b6fd95d768
Nikolas Stojcevich <nstojcevich@gmail.com>
2018-05-09 13:44:58 -0400

    ahh

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import corsheaders
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
