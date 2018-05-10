a4cafb0e20c2b0b86cea0563f6f94e460bf8118f
Nikolas Stojcevich <nstojcevich@gmail.com>
2018-05-10 13:55:31 -0400

    g

--- hello/views.py
+++ hello/views.py
@@ -53,6 +53,7 @@ def greetings(request):  # I think this code is from some example, but i'm leavi
 
 # Actual routes------------------------------------------------------------------
 def signup(request):
+    '''
     occupants = lotManager.getOccupants()
     vLot = lotManager.getLot()
     userCoords = lot.getVlotCoordinates(request.body.carLocation[0], request.body.carLocation[1])
@@ -67,6 +68,7 @@ def signup(request):
         "instructions": ["Go fast", "Turn left"],
         "exists": True
     }
+    '''
 
     test_response = {
         'Hello!'
