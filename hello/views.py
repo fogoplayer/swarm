a233211bb5a23286989e7ca3d8e26a148166729b
Zarin Loosli <fogoplayer@gmail.com>
2018-05-11 16:32:28 +0000

    Uncommented server code

--- hello/views.py
+++ hello/views.py
@@ -53,7 +53,6 @@ def greetings(request):  # I think this code is from some example, but i'm leavi
 
 # Actual routes------------------------------------------------------------------
 def signup(request):
-    '''
     occupants = lotManager.getOccupants()
     vLot = lotManager.getLot()
     userCoords = lot.getVlotCoordinates(request.body.carLocation[0], request.body.carLocation[1])
@@ -68,7 +67,6 @@ def signup(request):
         "instructions": ["Go fast", "Turn left"],
         "exists": True
     }
-    '''
 
     test_response = {
         'id':  7,
