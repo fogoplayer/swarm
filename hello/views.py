a53e23b00d7b1e28d506ecca5dc180d32736ffec
Zarin Loosli <fogoplayer@gmail.com>
2018-05-14 16:42:29 +0000

    Tweaked server

--- hello/views.py
+++ hello/views.py
@@ -57,9 +57,9 @@ def signup(request):
     occupants = lotManager.getOccupants()
     print(2)
     vLot = lotManager.getLot()
-    '''print(3)
+    print(3)
     print(request)
-    userCoords = lot.getVlotCoordinates(request.body.carLocation[0], request.body.carLocation[1])
+    '''userCoords = lot.getVlotCoordinates(request.body.carLocation[0], request.body.carLocation[1])
     print(4)
     dest = vLot[userCoords[0]][userCoords[1]].getDestination()
     occupants += [(request.body.carColor, request.body.carType, dest)]
@@ -68,7 +68,7 @@ def signup(request):
     lotManager.setOccupants(occupants)'''
     response = {
         'id': len(occupants) - 1,
-        "instructions": ["Go fast1", "Turn left"],
+        "instructions": ["Go fast2", "Turn left"],
         "exists": True
     }
 
