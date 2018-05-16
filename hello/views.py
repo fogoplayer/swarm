a5885e2cf49a441d16772d28653c908dd8429413
Zarin Loosli <fogoplayer@gmail.com>
2018-05-16 16:44:03 +0000

    Tweaked server

--- hello/views.py
+++ hello/views.py
@@ -66,7 +66,7 @@ def signup(request):
     lotManager.setOccupants(occupants)'''
     response = {
         'id': len(occupants) - 1,
-        "instructions": ["Go fast2", "Turn left", str(request), str(request.GET.get("carColor"))],
+        "instructions": ["Go fast2", "Turn left", str(request), str(request.GET.get("carLocation"))],
         "exists": True
     }
 
