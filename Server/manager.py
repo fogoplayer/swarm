98eceea3d2fcef27307f41ea62bd0f4adcb2f5bd
Zarin Loosli <fogoplayer@gmail.com>
2018-05-18 16:26:46 +0000

    Tweaked server

--- Server/manager.py
+++ Server/manager.py
@@ -33,7 +33,7 @@ class Spot:
 
 
 d = {'lot': [], 'occupants': []}
-print("Name: " + str(__name__));
+print("Name is Server " + str(__name__ == "Server.Manager"));
 if __name__ == "Server.manager":
     manager = Manager()
     d = manager.dict()
