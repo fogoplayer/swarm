092a0ada24268b6afc5625964b2cbbc2a7383cb5
Nikolas <nstojcevich@gmail.com>
2018-05-21 14:02:03 -0400

    update server code

--- Server/manager.py
+++ Server/manager.py
@@ -63,27 +63,13 @@ if __name__ == "Server.manager":
 
 def getLot():
     print("** GET LOT CALLED **")
-    if d['lot'] is not None:
-        print("d['lot'] = " + str(Manager.d['lot']))
-        return Manager.d['lot']
+    if Manager.__dict__['lot'] is not None:
+        print("d['lot'] = " + str(Manager.__dict__['lot']))
+        return Manager.__dict__['lot']
     else:
         print("d['lot'] has nothing in it")
         return
 
-
-d = {'lot': [], 'occupants': []}
-print("__name__" + __name__)
-manager = Manager()
-d = manager.dict()
-d['lot'] = [
-            [Spot(None , [None]       ,False , None), Spot(None , [None] ,True , None), Spot(None , [[1,2]]      ,True , None),  Spot(None , [None]       ,False , None)],
-            [Spot([1,1], [[2,0]]      ,True , None), Spot([1,2], [[1,0]],False , None), Spot([0,2], [[1,1],[1,3]],True , None),  Spot([1,3], [[2,3]]      ,True , None)],
-            [Spot([1,0], [[3,0],[2,1]],True , None), Spot([2,0], [None] ,False , None), Spot([2,3], [None]       ,False , None), Spot([1,3], [[3,3],[2,2]],True , None)],
-            [Spot([2,0], [[4,0],[3,1]],True , None), Spot([3,0], [None] ,False , None), Spot([3,3], [None]       ,False , None), Spot([2,3], [[4,3],[3,2]],True , None)],
-            [Spot([3,0], [[5,0],[4,1]],True , None), Spot([4,0], [None] ,False , None), Spot([4,3], [None]       ,False , None), Spot([3,3], [[5,3],[4,2]],True , None)],
-            [Spot([4,0], [None]       ,True , None), Spot(None , [None] ,False , None), Spot(None , [None]       ,False , None), Spot([4,3], [None]       ,True , None)]
-            ]
-d['occupants'] = []
 
 def setLot(lot):
     Manager.__dict__['lot'] = lot
